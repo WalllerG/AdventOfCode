49485a4e53aa68ff18e344f8050c7bdcb0e64c20
Walter Guo <guowalter61@gmail.com>
2026-02-08 14:25:54 -0500

    edit gitignore

--- src/Day1/Part1.java
+++ src/Day1/Part1.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 
-public class Dial {
+public class Part1 {
 
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new FileReader("src/Day1/operationList.txt"))) {
