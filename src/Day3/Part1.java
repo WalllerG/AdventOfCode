2f7ac419576e2ed419e0c46f083bc2cb3f13c872
Walter Guo <guowalter61@gmail.com>
2026-02-10 21:12:56 -0500

    edit day3

--- src/Day3/Part1.java
+++ src/Day3/Part1.java
@@ -1,4 +1,12 @@
 package Day3;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Part1 {
+    BufferedReader br = new BufferedReader(new FileReader("src/Day3/test.txt"));
+
+    public Part1() throws FileNotFoundException {
+    }
 }
