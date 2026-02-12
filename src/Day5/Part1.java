4ccd852867a460f85c19daebfbbe1fdfedb4c2ad
Walter Guo <guowalter61@gmail.com>
2026-02-12 10:24:01 -0500

    add day5

--- src/Day5/Part1.java
+++ src/Day5/Part1.java
@@ -1,4 +1,11 @@
 package Day5;
 
+import advent.util.Util;
+import java.util.List;
+
 public class Part1 {
+    static void main(String[] args) throws Exception {
+        List<String> lines = Util.readInput(false,5);
+
+    }
 }
--- src/Day5/Part2.java
+++ src/Day5/Part2.java
@@ -1,4 +1,12 @@
 package Day5;
 
+import advent.util.Util;
+
+import java.util.List;
+
 public class Part2 {
+    static void main(String[] args) throws Exception {
+        List<String> lines = Util.readInput(false,5);
+
+    }
 }
