f5556f5e4fbbdc6561544fcd71d928de9663561f
Walter Guo <guowalter61@gmail.com>
2026-02-13 16:36:32 -0500

    finish Day6!

--- src/Day6/Part2.java
+++ src/Day6/Part2.java
@@ -3,31 +3,84 @@ package Day6;
 import advent.util.Util;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Part2 {
     static void main() throws IOException {
 
-        List<String> lines = Util.readInput(false,6);
-        List<List<String>> rows = new ArrayList<>();
-        for (String line : lines) {
-            rows.add(Part1.getRow(line));
-        }
-        List<List<String>> cols = getColumn(rows);
-        System.out.println(Arrays.deepToString(cols.toArray()));
-    }
-
-    public static List<List<String>> getColumn (List<List<String>> input) {
-        List<List<String>> result = new ArrayList<>();
-        for (int i = 0; i < input.getFirst().size(); i++) {
-            List<String> list = new ArrayList<>();
-            for (List<String> strings : input) {
-                list.add(strings.get(i));
+        List<String> lines = Util.readInput(true, 6);
+        List<String[]> list = lines.stream().map(line -> line.split("")).collect(Collectors.toList());
+        int max = 0;
+        for (String[] strings : list) {
+            if (strings.length > max) {
+                max = strings.length;
             }
-            result.add(list);
         }
-        return result;
+
+        for (int i = 0; i < list.size(); i++) {
+            String[] current = list.get(i);
+            if (current.length < max) {
+                String[] padded = Arrays.copyOf(current, max);
+
+                for (int j = current.length; j < max; j++) {
+                    padded[j] = " ";
+                }
+                list.set(i, padded);
+            }
+        }
+
+        List<Long> numList = new ArrayList<>();
+        List<List<Long>> finalList = new ArrayList<>();
+        outer:
+        for (int j = 0; j < max; j++) {
+            String newNum = "";
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(0)[j].equals(" ") && list.get(1)[j].equals(" ") && list.get(2)[j].equals(" ") && list.get(3)[j].equals(" ")) {
+                    finalList.add(numList);
+                    numList = new ArrayList<>();
+                    continue outer;
+                }
+                if (list.get(i)[j].equals(" ") || list.get(i)[j] == null) {
+                    newNum += "";
+                } else {
+                    newNum += list.get(i)[j];
+                }
+            }
+            long newLong = Long.parseLong(newNum);
+            numList.add(newLong);
+            if (j == max - 1) {
+                finalList.add(numList);
+            }
+        }
+
+        String[] signs = lines.getLast().split(" ");
+        List<String> signList = new ArrayList<>();
+        for (String sign : signs) {
+            if (sign.contains("*")) {
+                signList.add("*");
+            } else if (sign.contains("+")) {
+                signList.add("+");
+            }
+        }
+
+        long finalResult = 0;
+        for  (int i = 0; i < signList.size(); i++) {
+            if  (signList.get(i).equals("*")) {
+                long time = 1;
+                for (Long l : finalList.get(i)) {
+                    time *= l;
+                }
+                finalResult += time;
+            }
+            else if(signList.get(i).equals("+")) {
+                long add = 0;
+                for (Long l : finalList.get(i)) {
+                    add += l;
+                }
+                finalResult += add;
+            }
+        }
+        System.out.println(finalResult);
     }
 }
