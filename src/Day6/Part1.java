0a271e617c23fde8bd72ad6dd028da1cc9eaf168
Walter Guo <guowalter61@gmail.com>
2026-02-13 10:15:29 -0500

    finish day6 part1

--- src/Day6/Part1.java
+++ src/Day6/Part1.java
@@ -2,23 +2,19 @@ package Day6;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import advent.util.Util;
 
 public class Part1 {
-    public static void main(String[] args) throws IOException {
+    static void main() throws IOException {
 
         List<String> lines = Util.readInput(true,6);
-        List<List<String>> list = new ArrayList<>();
-        for (int i = 0; i < lines.size(); i++) {
-            List<String> row = getRow(lines.get(i));
-            list.add(row);
-        }
-        System.out.println(list);
+        List<List<String>> list = lines.stream().map(Part1::getRow).collect(Collectors.toList());
         long result = 0;
         for  (int i = 0; i < list.getFirst().size(); i++) {
-            String operation = list.get(list.size()-1).get(i);
+            String operation = list.getLast().get(i);
             long time = 1;
             long add = 0;
             for (int j = 0; j < list.size()-1; j++) {
@@ -42,11 +38,8 @@ public class Part1 {
      public static List<String> getRow (String input) {
         String[] nums = input.split(" ");
         List<String> list = new ArrayList<>();
-        outer:
          for (String num : nums) {
-             if (num.isEmpty()) {
-                 continue outer;
-             } else {
+             if (!num.isEmpty()) {
                  list.add(num);
              }
          }
