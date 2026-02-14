d20a10dc0b3362846534e6f911eaf2be1450c42b
Walter Guo <guowalter61@gmail.com>
2026-02-13 22:16:27 -0500

    finish day7 part1

--- src/Day7/Part1.java
+++ src/Day7/Part1.java
@@ -2,18 +2,48 @@ package Day7;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class Part1 {
-    static void main() throws Exception{
-        List<String > lines = advent.util.Util.readInput(false,7);
+    static void main() throws Exception {
+        List<String> lines = advent.util.Util.readInput(true, 7);
         List<List<String>> simplify = new ArrayList<>();
-        for (String line : lines){
-            List<String> row = Arrays.asList(line.split(" "));
-            if (row.contains("^")) {
+        for (String line : lines) {
+            List<String> row = Arrays.asList(line.split(""));
+            if (row.contains("^") || row.contains("S")) {
                 simplify.add(row);
             }
         }
-        System.out.println(simplify);
+        HashSet<Integer> SIndex = new HashSet<>();
+        int Start = getStartIndex(simplify.getFirst());
+        SIndex.add(Start);
+        int count = 0;
+        for (int i = 0; i < simplify.size() - 1; i++) {
+            HashSet<Integer> copy = deepCopy(SIndex);
+            for (Integer index : copy) {
+                if (simplify.get(i + 1).get(index).equals("^")) {
+                    SIndex.remove(index);
+                    SIndex.add(index + 1);
+                    SIndex.add(index - 1);
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+
+    public static HashSet<Integer> deepCopy(HashSet<Integer> SIndex) {
+        return new HashSet<>(SIndex);
+    }
+
+    public static int getStartIndex(List<String> lines) {
+        for (int i = 1; i < lines.size(); i++) {
+            if (lines.get(i).equals("S")) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
