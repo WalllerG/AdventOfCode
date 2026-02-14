debfa1f938497729c099a5c2ea7c868b936839bc
Walter Guo <guowalter61@gmail.com>
2026-02-13 21:33:52 -0500

    add day7

--- src/Day7/Part1.java
+++ src/Day7/Part1.java
@@ -1,4 +1,19 @@
 package Day7;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Part1 {
+    static void main() throws Exception{
+        List<String > lines = advent.util.Util.readInput(false,7);
+        List<List<String>> simplify = new ArrayList<>();
+        for (String line : lines){
+            List<String> row = Arrays.asList(line.split(" "));
+            if (row.contains("^")) {
+                simplify.add(row);
+            }
+        }
+        System.out.println(simplify);
+    }
 }
