6abdc99c98f2d82df9a870d2ad1d53a86db5a26a
Walter Guo <guowalter61@gmail.com>
2026-02-14 17:52:21 -0500

    add day8

---
+++ src/Day8/Part1.java
@@ -0,0 +1,13 @@
+package Day8;
+
+import advent.util.Util;
+import java.io.IOException;
+import java.util.List;
+
+public class Part1 {
+    public static void main() throws IOException {
+        List<String > lines = Util.readInput(false,8);
+
+
+    }
+}
