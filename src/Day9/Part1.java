f0b081aa6cf1481d7cf8496b9aff2735c10b3e6b
Walter Guo <guowalter61@gmail.com>
2026-02-16 08:29:57 -0500

    add day9

--- src/Day9/Part1.java
+++ src/Day9/Part1.java
@@ -1,4 +1,19 @@
 package Day9;
 
+import advent.util.Util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Part1 {
+    static void main() throws Exception{
+        List<String> lines = Util.readInput(false,9);
+        List<int[]> coors = new ArrayList<>();
+        for (int i = 0 ; i < lines.size() ; i++) {
+            String[] coor = lines.get(i).split(",");
+            coors.add(new int[] {Integer.parseInt(coor[0]), Integer.parseInt(coor[1])});
+        }
+        System.out.println(Arrays.deepToString(coors.toArray()));
+    }
 }
