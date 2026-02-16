e9917eb861e7d298cd7c5d8d60ede55b5ab9ceeb
Walter Guo <guowalter61@gmail.com>
2026-02-15 22:30:50 -0500

    finish day8!

--- src/Day8/Part2.java
+++ src/Day8/Part2.java
@@ -4,15 +4,38 @@ import advent.util.Util;
 import java.io.IOException;
 import java.util.*;
 
+import static Day8.Part1.getConnectedGroups;
+
 public class Part2 {
     static void main() throws IOException {
         List<String > lines = Util.readInput(true,8);
         HashMap<Integer, List<Integer>> junctionBoxes = Part1.getJunctionBoxes(lines);
         List<Map.Entry<Integer,List<Integer>>> sortedList = sort(junctionBoxes);
-        List<int[]> pair = Part1.findClosestKeys(sortedList,5471);
-        long result;
-        result = (long) junctionBoxes.get(362).getFirst() * junctionBoxes.get(954).getFirst();
+        Set<Integer> allKeys = new HashSet<>();
+        for (int i = 0; i < 1000; i++) {
+            allKeys.add(i);
+        }
+        List<List<Integer>> circuits;
+        int end = 6000;
+        int start = 1000;
+        int startBefore = 1000;
+        circuits = getConnectedGroups(Part1.findClosestKeys(sortedList,start),allKeys);
+        while (end - start > 1) {
+            if (circuits.size() > 1) {
+                startBefore = start;
+                start = start + (end - start) / 2;
+                circuits = getConnectedGroups(Part1.findClosestKeys(sortedList,start),allKeys);
+            }
+            else {
+                end = start;
+                start = startBefore;
+                circuits = getConnectedGroups(Part1.findClosestKeys(sortedList,start),allKeys);
+            }
+        }
+        List<int[]> pair = Part1.findClosestKeys(sortedList,start);
+        long result = (long) junctionBoxes.get(pair.getLast()[0]).getFirst() * junctionBoxes.get(pair.getLast()[1]).getFirst();
         System.out.println(result);
+
     }
 
     public static List<Map.Entry<Integer, List<Integer>>> sort (HashMap<Integer, List<Integer>> junctionBoxes) {
