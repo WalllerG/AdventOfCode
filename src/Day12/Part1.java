bde9b683de7800a07822bc0c26f4932702685c87
Walter Guo <guowalter61@gmail.com>
2026-02-20 17:48:18 -0500

    add day12

--- src/Day12/Part1.java
+++ src/Day12/Part1.java
@@ -1,4 +1,12 @@
 package Day12;
 
+import advent.util.Util;
+import java.util.List;
+
 public class Part1 {
+    public static void main(String[] args) throws Exception {
+        List<String> lines = Util.readInput(false, 11);
+
+
+    }
 }
