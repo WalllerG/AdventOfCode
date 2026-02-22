c4d9ca025f04a56abf57db6cef937b6c8c0ab99c
Walter Guo <guowalter61@gmail.com>
2026-02-21 23:42:48 -0500

    finish 2025!

--- src/Day12/Region.java
+++ src/Day12/Region.java
@@ -1,15 +1,9 @@
 package Day12;
 
-import java.util.Arrays;
 
 public class Region {
     boolean[][] region;
     public Region(int  col, int row) {
-        this.region = new boolean[row][col];;
-    }
-    public void printRegion () {
-        for (boolean[] booleans : this.region) {
-            System.out.println(Arrays.toString(booleans));
-        }
+        this.region = new boolean[row][col];
     }
 }
