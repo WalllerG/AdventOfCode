d76c2903208455da524cbdf1f572263ac41887c2
WalllerG <guowalter61@gmail.com>
2026-02-22 15:40:43 -0500

    add day2

---
+++ 2024/Day2/Part1.py
@@ -0,0 +1,26 @@
+from Util.util import read_input
+
+data = read_input(2, True)
+
+result = 0
+for line in data:
+    nums = line.split(" ")
+    increase = int (nums[0]) < int (nums[1])
+    decrease = int (nums[0]) > int (nums[1])
+    safe = True
+    for i in range (0,len(nums)-1):
+        if decrease:
+            if int (nums[i]) > int (nums[i+1]):
+                if int(nums[i]) - int(nums[i+1]) < 1 or int (nums[i]) -int (nums[i+1]) > 3:
+                    safe = False
+            else:
+                break
+        elif increase:
+            if int (nums[i]) < int (nums[i+1]):
+                if int (nums[i]) - int(nums[i+1]) > 3 or int (nums[i]) -int (nums[i+1]) < 1:
+                    safe = False
+            else:
+                break
+    if safe:
+        result += 1
+print(result)
