efb62ca3716ea49c11fcad3f9fae6944b44aaa5a
Walter Guo <guowalter61@gmail.com>
2026-02-23 11:19:50 -0500

    finish day3!

--- 2024/Day3/Part2.py
+++ 2024/Day3/Part2.py
@@ -4,18 +4,15 @@ import re
 
 data = read_input(3, True)
 result = 0
+enable = True
 for line in data:
     combined_pattern = r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)"
     matches = re.finditer(combined_pattern, line)
     for match in matches:
         full_match = match.group(0)
-        enable = False
-        disable = False
         if full_match == "do()":
             enable = True
-            disable = False
         elif full_match == "don't()":
-            disable = True
             enable = False
         else :
             if enable:
