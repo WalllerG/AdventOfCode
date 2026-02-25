6fe29aac0e4453569cacc3b9a8dd648f958f0251
Walter Guo <guowalter61@gmail.com>
2026-02-24 22:57:44 -0500

    finish day5 Part1.py

--- 2024/Day5/Part1.py
+++ 2024/Day5/Part1.py
@@ -1,10 +1,34 @@
 from Util.util import read_input
-data = read_input(5,False)
+data = read_input(5,True)
 index = 0
 IntMap = {}
 while data[index] != "":
     line = data[index]
     pair = line.split("|")
-    IntMap[pair[0]] = int(pair[1])
+    key = pair[0]
+    value = pair[1]
+    if key in IntMap:
+        IntMap[key].append(value)
+    else:
+        IntMap[key] = [value]
     index += 1
-print(IntMap)
+
+result = 0
+
+for i in range(index+1,len(data)):
+    line = data[i]
+    nums = line.split(",")
+    isRightOrder = True
+    for j in range(len(nums)-1):
+        if nums[j] not in IntMap:
+            isRightOrder = False
+            break
+        elif nums[j+1] not in IntMap[nums[j]]:
+            isRightOrder = False
+            break
+    if isRightOrder:
+        index = int (len(nums) / 2)
+        result += int (nums[index])
+
+
+print(result)
