789f2fce848f1d3f7c7e9bec05fad1268f04a270
Walter Guo <guowalter61@gmail.com>
2026-02-28 09:36:25 -0500

    finish day7!

---
+++ 2024/Day7/Part2.py
@@ -0,0 +1,44 @@
+
+from Util.util import read_input
+
+data = read_input(7,True)
+
+numsMap = {}
+
+for line in data:
+    target = int (line.split(":")[0])
+    nums_str = line.split(":")[1]
+    nums = [int (x) for x in nums_str.split()]
+    numsMap[target] = nums
+
+def concatenation (a:int, b:int) -> int:
+    str_a = str(a)
+    str_b = str(b)
+    return int(str_a + str_b)
+
+
+def backTrack(entry: tuple, count: int, current: int) -> bool:
+
+    key, value = entry
+    if count == len(value):
+        return current == key
+
+    next_num = value[count]
+    if backTrack(entry, count + 1, current * next_num):
+        return True
+    if backTrack(entry, count + 1, current + next_num):
+        return True
+    if backTrack(entry, count + 1, concatenation(current, next_num)):
+        return True
+
+    return False
+
+entries = list(numsMap.items())
+result = 0
+for entry in entries:
+    if backTrack(entry, 1, entry[1][0]):
+        result += entry[0]
+
+
+print(result)
+
