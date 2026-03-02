778131415a04146fe62591f6b010bb4b5251555b
Walter Guo <guowalter61@gmail.com>
2026-03-01 19:47:51 -0500

    finish day10 Part1.py

--- 2024/Day10/Part1.py
+++ 2024/Day10/Part1.py
@@ -1,4 +1,41 @@
 from Util.util import read_input
 
-data = read_input(10, False)
+data = read_input(10, True)
+result = 0
+trailheads = []
 
+grid = []
+for line in data:
+    lst = list(line)
+    grid.append(lst)
+
+for i in range(len(grid)):
+    for j in range(len(grid[i])):
+        if grid[i][j] == "0":
+            trailheads.append([i, j])
+
+def backtrack (current,trail_tails):
+    num = int(grid[current[0]][current[1]])
+    if current not in trail_tails and grid[current[0]][current[1]] == "9":
+        trail_tails.append(current)
+
+    up = current[0]-1
+    down = current[0]+1
+    left = current[1]-1
+    right = current[1]+1
+
+    if -1 < up < len(grid) and int (grid[up][current[1]]) - num == 1:
+        backtrack([up,current[1]],trail_tails)
+    if -1 < down < len(grid) and int (grid[down][current[1]]) - num == 1:
+        backtrack([down,current[1]],trail_tails)
+    if -1 < left < len(grid) and int (grid[current[0]][left]) - num == 1:
+        backtrack([current[0],left],trail_tails)
+    if -1 < right < len(grid) and int (grid[current[0]][right]) - num == 1:
+        backtrack([current[0],right],trail_tails)
+
+for head in trailheads:
+    lst = []
+    backtrack(head,lst)
+    result += len(lst)
+
+print(result)
