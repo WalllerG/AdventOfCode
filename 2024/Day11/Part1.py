adfe981f8dd9117ab8a97013b09226a66420d345
Walter Guo <guowalter61@gmail.com>
2026-03-02 16:00:39 -0500

    finish day11!

--- 2024/Day11/Part1.py
+++ 2024/Day11/Part1.py
@@ -1,30 +1,31 @@
+from collections import Counter
+
 from Util.util import read_input
 
 data = read_input(11, True)
+stone_counts = Counter(map(int, data[0].split()))
 
-num_of_Stones = data[0].split(" ")
+def solve(counts, blinks):
+    for _ in range(blinks):
+        new_counts = Counter()
+        for stone, qty in counts.items():
 
-def blinking (lst) -> list[str]:
-    new_num_of_stones = []
-    for stone in lst:
-        if stone == "0":
-            new_num_of_stones.append("1")
-        elif len(stone) % 2 == 0:
-            left = int(stone[:len(stone)//2])
-            right = int(stone[len(stone)//2:])
-            new_num_of_stones.append(str(left))
-            new_num_of_stones.append(str(right))
-        else:
-            new_num = int (stone) * 2024
-            new_num_of_stones.append(str(new_num))
+            if stone == 0:
+                new_counts[1] += qty
 
-    return new_num_of_stones
+            elif len(str(stone)) % 2 == 0:
+                s = str(stone)
+                mid = len(s) // 2
+                left = int(s[:mid])
+                right = int(s[mid:])
+                new_counts[left] += qty
+                new_counts[right] += qty
 
-def recursion (count, num_of_stones, target) -> int:
-    if count == target:
-        return len(num_of_stones)
-    else:
-        new_num_of_stones = blinking(num_of_stones)
-        return recursion(count + 1, new_num_of_stones, target)
+            else:
+                new_counts[stone * 2024] += qty
 
-print(recursion(0, num_of_Stones, 25))
+        counts = new_counts
+
+    return sum(counts.values())
+
+print(solve(stone_counts, 25))
---
+++ 2024/Day11/Part2.py
@@ -0,0 +1,31 @@
+from collections import Counter
+
+from Util.util import read_input
+
+data = read_input(11, True)
+stone_counts = Counter(map(int, data[0].split()))
+
+def solve(counts, blinks):
+    for _ in range(blinks):
+        new_counts = Counter()
+        for stone, qty in counts.items():
+
+            if stone == 0:
+                new_counts[1] += qty
+
+            elif len(str(stone)) % 2 == 0:
+                s = str(stone)
+                mid = len(s) // 2
+                left = int(s[:mid])
+                right = int(s[mid:])
+                new_counts[left] += qty
+                new_counts[right] += qty
+
+            else:
+                new_counts[stone * 2024] += qty
+
+        counts = new_counts
+
+    return sum(counts.values())
+
+print(solve(stone_counts, 75))
