93c781948a4d5eaae37f0d584231e809de334bac
Walter Guo <guowalter61@gmail.com>
2026-03-01 20:27:19 -0500

    edit day10 Part2.java

--- 2025/src/Day10/Part2.java
+++ 2025/src/Day10/Part2.java
@@ -2,9 +2,11 @@ package Day10;
 public class Part2 {
     public static void main()
     {
-        //TODO SINCE It's too hard to use a non-library to solve this part, I used Or-Tool to solve this question
-        //TODO I have created a repo on github about the matrix solver with maven
-        //TODO To clone the project simple clone the url below
-        //TODO
+        /*
+         TODO SINCE It's too hard to use a non-library to solve this part, I used Or-Tool to solve this question
+         TODO I have created a repo on github about the matrix solver with maven
+         TODO To clone the project simply clone the url below
+         TODO https://github.com/WalllerG/MatrixSolver.git
+         */
     }
 }
