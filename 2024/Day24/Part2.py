0f21cad208ac237d04992737b566902e95a722a5
Walter Guo <guowalter61@gmail.com>
2026-03-07 22:40:00 -0500

    finish day24!

--- 2024/Day24/Part2.py
+++ 2024/Day24/Part2.py
@@ -1,10 +1,7 @@
 from Util.util import read_input
 data = read_input(24,True)
 wires = {}
-x_lst = ""
-y_lst = ""
-n2s = []
-xor_wires = []
+operations = set()
 
 count = 0
 for line in data:
@@ -13,39 +10,12 @@ for line in data:
     else:
         name, value = line.split(":")
         wires[name] = int(value)
-        if name.startswith("x"):
-            x_lst += value.strip()
-        elif name.startswith("y"):
-            y_lst += value.strip()
     count += 1
 
-operations = set()
 for i in range(count+1, len(data)):
     operation = data[i].split(" ")
-    op = (operation[0], operation[1], operation[2], operation[4])
-    operations.add(op)
-    if operation[4].startswith("z") and operation[1] != "XOR":
-        n2s.append(op)
-    if not operation[4].startswith("z") and operation[1] == "XOR":
-        xor_wires.append(op)
-
-def compute(left, o, right, des):
-    if o == "AND":
-        wires[des] = wires[left] & wires[right]
-    elif o == "OR":
-        wires[des] = wires[left] | wires[right]
-    elif o == "XOR":
-        wires[des] = wires[left] ^ wires[right]
-
-def wiring(op_lst):
-    while len(op_lst) !=0 :
-        cur = op_lst.pop()
-        l, op, r, d = cur[0], cur[1], cur[2], cur[3]
-        if l not in wires or r not in wires:
-            operations.add((l, op, r, d))
-        else:
-            compute(l,op,r,d)
-
+    ope = (operation[0], operation[1], operation[2], operation[4])
+    operations.add(ope)
 
 def find_swap_pairs(gates):
     swapped = set()
@@ -64,8 +34,8 @@ def find_swap_pairs(gates):
                         swapped.add(out)
 
         if op == 'AND' and "00" not in l and "00" not in r:
-            for next_in1, next_op, next_in2, next_out in gates:
-                if (out == next_in1 or out == next_in2) and next_op != 'OR':
+            for nl, nop, nr, n_out in gates:
+                if (out == nl or out == nr) and nop != 'OR':
                     swapped.add(out)
 
     return ",".join(sorted(swapped))
