8532c559dad6acbebab2be3590d4a8dcafc75679
WalllerG <guowalter61@gmail.com>
2026-03-08 18:00:50 -0400

    finish day4

---
+++ 2023/Day04/Part1.py
@@ -0,0 +1,19 @@
+from Util.util import read_input
+data = read_input(4,True)
+
+ans = 0
+for line in data:
+    nums = line.split(":")[-1]
+    win_nums, num_to_match = nums.strip().split("|")
+    wns = [int(x) for x in win_nums.split()]
+    to_match = [int(y) for y in num_to_match.split()]
+    count = 0
+    for wn in wns:
+        if wn in to_match:
+            if count == 0:
+                count += 1
+            else:
+                count *= 2
+    ans += count
+
+print(ans)
---
+++ 2023/Day04/Part2.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+from Util.util import read_input
+data = read_input(4,True)
+
+cards = defaultdict(int)
+
+ans = 0
+for i in range(len(data)):
+    nums = data[i].split(":")[-1]
+    win_nums, num_to_match = nums.strip().split("|")
+    wns = [int(x) for x in win_nums.split()]
+    to_match = [int(y) for y in num_to_match.split()]
+    count = 0
+    for wn in wns:
+        if wn in to_match:
+           count += 1
+
+    for j in range(0, count):
+        cards[i+j+1] += 1
+
+    if count == 0:
+        cards[i] += 0
+    if count != 0:
+        for k in range(0, cards[i]):
+            count = 0
+            for wn in wns:
+                if wn in to_match:
+                    count += 1
+
+            for j in range(0, count):
+                cards[i + j + 1] += 1
+
+
+for v in cards.values():
+    ans += v+1
+print(ans)
