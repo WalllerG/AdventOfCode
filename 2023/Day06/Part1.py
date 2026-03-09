89a932e65fcded13bb43da4d5e0095d59d1c39ce
WalllerG <guowalter61@gmail.com>
2026-03-08 23:48:24 -0400

    finish day6

---
+++ 2023/Day06/Part1.py
@@ -0,0 +1,24 @@
+import re
+
+from Util.util import read_input
+
+data = read_input(True)
+ans = 1
+times = {}
+
+time = [int(x) for x in re.findall(r'\d+', data[0])]
+distance = [int(x) for x in re.findall(r'\d+', data[1])]
+
+for t, d in zip(time, distance):
+    times[t] = d
+
+for k, v in times.items():
+    way = 0
+    for i in range (0, k):
+        speed = k - i
+        dis = i * speed
+        if dis > v:
+            way += 1
+    ans *= way
+
+print(ans)
---
+++ 2023/Day06/Part2.py
@@ -0,0 +1,24 @@
+import re
+
+from Util.util import read_input
+data = read_input(True)
+ans = 0
+
+time = [int(x) for x in re.findall(r'\d+', data[0])]
+distance = [int(x) for x in re.findall(r'\d+', data[1])]
+
+t = ""
+d = ""
+for t1 in time:
+    t += str(t1)
+for t2 in distance:
+    d += str(t2)
+t = int (t)
+d = int (d)
+
+for i in range(t):
+    speed = t - i
+    dis = speed * i
+    if dis > d:
+        ans += 1
+print(ans)
