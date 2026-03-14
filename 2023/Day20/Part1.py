8d2b6570859525fef32f9e66c62db0322c432517
WalllerG <guowalter61@gmail.com>
2026-03-14 01:03:07 -0400

    1

--- 2023/Day20/Part1.py
+++ 2023/Day20/Part1.py
@@ -1,15 +1,18 @@
+from collections import defaultdict
 from Util.util import read_input
-data = read_input(False)
+data = read_input(True)
 modulos = {}
 flip_flops = {}
-inputs = {}
+inputs = defaultdict(list)
+conjunction_memo ={}
+
 for line in data:
     p1,target = line.split(" -> ")
     if p1.startswith("b"):
         targets = list(target.split(", "))
         modulos[p1] = ("b", targets)
         for target in targets:
-            inputs[target] = p1
+            inputs[target].append(p1)
     else:
         kind, name = p1[0], p1[1:]
         if kind == "%":
@@ -17,47 +20,63 @@ for line in data:
         targets = list(target.split(", "))
         modulos[name] = (kind, targets)
         for target in targets:
-            inputs[target] = name
+            inputs[target].append(name)
+
+for n, mo in modulos.items():
+    con_dict = {}
+    if mo[0] == "&":
+        for inp in inputs[n]:
+            con_dict[inp] = False
+        conjunction_memo[n] = con_dict
 
 low_pulse = 0
 high_pulse = 0
-for i in range(1):
-    low_pulse += 1
+for i in range(1000):
     queue = [("broadcaster", 0)]
     while queue:
         cur_mod, pulse = queue.pop(0)
-        if cur_mod == "output":
-            if pulse == 0:
-                low_pulse += 1
-            else:
-                high_pulse += 1
+        if pulse == 0:
+            low_pulse += 1
+        if pulse == 1:
+            high_pulse += 1
+
+        if cur_mod == "rx":
             continue
         type, targets = modulos[cur_mod][0], modulos[cur_mod][1]
         if type == "b":
             for target in targets:
                 queue.append((target, 0))
-                low_pulse += 1
         if type == "%":
-            for target in targets:
-                if pulse == 0 and not flip_flops[cur_mod]:
-                    flip_flops[cur_mod] = not flip_flops[cur_mod]
-                    queue.append((target, 1))
-                    high_pulse += 1
-                elif pulse == 0 and flip_flops[cur_mod]:
-                    flip_flops[cur_mod] = not flip_flops[cur_mod]
-                    queue.append((target, 0))
-                    low_pulse += 1
-                elif pulse == 1:
-                    continue
-        if type == "&":
-            for target in targets:
-                if pulse == 0:
-                    queue.append((target, 1))
-                    high_pulse += 1
-                else:
-                    queue.append((target, 0))
-                    low_pulse += 1
+            if pulse == 0:
+                for target in targets:
+                    if target != "rx":
+                        if modulos[target][0] == "&":
+                            conjunction_memo[target][cur_mod] = not conjunction_memo[target][cur_mod]
+                    if not flip_flops[cur_mod]:
+                        queue.append((target, 1))
+                    elif flip_flops[cur_mod]:
+                        queue.append((target, 0))
+                flip_flops[cur_mod] = not flip_flops[cur_mod]
+            elif pulse == 1:
+                continue
 
-print(low_pulse , high_pulse)
+        if type == "&":
+            if set(conjunction_memo[cur_mod].values()) != {True}:
+                for target in targets:
+                    if target != "rx":
+                        if modulos[target][0] == "&":
+                            if not conjunction_memo[target][cur_mod]:
+                                conjunction_memo[target][cur_mod] = True
+                    queue.append((target, 1))
+            else:
+                for target in targets:
+                    if target != "rx":
+                        if modulos[target][0] == "&":
+                            if conjunction_memo[target][cur_mod]:
+                                conjunction_memo[target][cur_mod] = False
+                    queue.append((target, 0))
+
+
+print(low_pulse * high_pulse)
 
 
---
+++ 2023/Day20/Part2.py
@@ -0,0 +1,99 @@
+import math
+from collections import defaultdict
+from Util.util import read_input
+data = read_input(True)
+modulos = {}
+flip_flops = {}
+inputs = defaultdict(list)
+conjunction_memo = {}
+
+for line in data:
+    p1,target = line.split(" -> ")
+    if p1.startswith("b"):
+        targets = list(target.split(", "))
+        modulos[p1] = ("b", targets)
+        for target in targets:
+            inputs[target].append(p1)
+    else:
+        kind, name = p1[0], p1[1:]
+        if kind == "%":
+            flip_flops[name] = False
+        targets = list(target.split(", "))
+        modulos[name] = (kind, targets)
+        for target in targets:
+            inputs[target].append(name)
+
+for n, mo in modulos.items():
+    con_dict = {}
+    if mo[0] == "&":
+        for inp in inputs[n]:
+            con_dict[inp] = False
+        conjunction_memo[n] = con_dict
+
+low_pulse = 0
+high_pulse = 0
+found_presses = False
+presses = 0
+cycle_length = {}
+seen = {key: 0 for key in ['js', 'qs', 'dt', 'ts']}
+
+while True:
+    presses += 1
+    queue = [("broadcaster", 0)]
+    while queue:
+        cur_mod, pulse = queue.pop(0)
+
+        if cur_mod == "rx":
+            continue
+
+        type, targets = modulos[cur_mod][0], modulos[cur_mod][1]
+
+        if "cl" in targets:
+            if set(conjunction_memo[cur_mod].values()) != {True}:
+                seen[cur_mod] += 1
+                if cur_mod not in cycle_length:
+                    cycle_length[cur_mod] = presses
+                else:
+                    assert presses == seen[cur_mod] * cycle_length[cur_mod]
+
+                if all(seen.values()):
+                    x = 1
+                    for v in cycle_length.values():
+                        x = math.lcm(x, v)
+                    print(x)
+                    exit(0)
+
+        if type == "b":
+            for target in targets:
+                queue.append((target, 0))
+
+        if type == "%":
+            if pulse == 0:
+                for target in targets:
+                    if target != "rx":
+                        if modulos[target][0] == "&":
+                            conjunction_memo[target][cur_mod] = not conjunction_memo[target][cur_mod]
+                    if not flip_flops[cur_mod]:
+                        queue.append((target, 1))
+                    elif flip_flops[cur_mod]:
+                        queue.append((target, 0))
+                flip_flops[cur_mod] = not flip_flops[cur_mod]
+            elif pulse == 1:
+                continue
+
+        if type == "&":
+            if set(conjunction_memo[cur_mod].values()) != {True}:
+                for target in targets:
+                    if target != "rx":
+                        if modulos[target][0] == "&":
+                            if not conjunction_memo[target][cur_mod]:
+                                conjunction_memo[target][cur_mod] = True
+                    queue.append((target, 1))
+            else:
+                for target in targets:
+                    if target != "rx":
+                        if modulos[target][0] == "&":
+                            if conjunction_memo[target][cur_mod]:
+                                conjunction_memo[target][cur_mod] = False
+                    queue.append((target, 0))
+
