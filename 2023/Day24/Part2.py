a2ca0a99989675065c8d996df0aa3a6e99b628dc
WalllerG <guowalter61@gmail.com>
2026-03-15 23:02:44 -0400

    finish day24

--- 2023/Day24/Part2.py
+++ 2023/Day24/Part2.py
@@ -1,8 +1,6 @@
 from Util.util import read_input
 data = read_input = read_input(True)
-from z3 import *
-least = 200000000000000
-most = 400000000000000
+import sympy
 hailstones = []
 for i in range(len(data)):
     pos, velocity = data[i].split(" @ ")
@@ -10,39 +8,14 @@ for i in range(len(data)):
     vx, vy, vz = [int(x) for x in velocity.split(", ")]
     hailstones.append(((x, y, z), (vx, vy, vz)))
 
-
 def solve(stones):
-    solver = Solver()
-    vars = []
-    x = Int('x')
-    y = Int('y')
-    z = Int('z')
-    vx = Int('vx')
-    vy = Int('vy')
-    vz = Int('vz')
-    solver.add(x >= 0)
-    solver.add(y >= 0)
-    solver.add(z >= 0)
-    for i in range(len(stones)):
-        t = Int("t{}".format(i))
-        vars.append(t)
-        (cx, cy, cz), (dx, dy, dz) = stones[i]
-        solver.add(cx + dx * t == x + vx * t)
-        solver.add(cy + dy * t == y + vy * t)
-        solver.add(cz + dz * t == z + vz * t)
-
-    for var in vars:
-        solver.add(var >= 0)
-    for i in range(len(vars)):
-        for j in range(i + 1, len(vars)):
-            solver.add(vars[i] != vars[j])
-
-    solver.check()
-    m = solver.model()
-    x_val = m[x].as_long()
-    y_val = m[y].as_long()
-    z_val = m[z].as_long()
-    return x_val + y_val + z_val
+    equation = []
+    xr, yr, zr, vxr, vyr, vzr = sympy.symbols("xr, yr, zr, vxr, vyr, vzr")
+    for (sx, sy, sz),(vx, vy, vz) in stones:
+        equation.append((xr - sx) * (vy - vyr) - (yr - sy) * (vx - vxr))
+        equation.append((yr- sy) * (vz - vzr) - (zr - sz) * (vy - vyr))
+    ans = sympy.solve(equation)
+    return ans[0][xr] + ans[0][yr] + ans[0][zr]
 
 print(solve(hailstones))
 
