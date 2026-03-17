6ff8a872c3e71d783c8e5aecfc0337b9ea136bc3
WalllerG <guowalter61@gmail.com>
2026-03-17 16:55:09 -0400

    6

---
+++ 2022/Day06/Part2.py
@@ -0,0 +1,10 @@
+with open("input.txt") as f:
+    data = f.read()
+line = list(data)
+start_pos = 14
+for i in range(len(line)):
+    if len(set(line[i:start_pos])) == 14:
+        print(start_pos)
+        break
+    start_pos += 1
+
