442c1c76099a905dfccec9c07869c72521298542
WalllerG <guowalter61@gmail.com>
2026-03-21 13:39:44 -0400

    20

---
+++ 2022/Day20/Part1.py
@@ -0,0 +1,18 @@
+with open("input.txt") as f:
+    data = list(map(int, f.read().splitlines()))
+ans = 0
+indexed_data = list(enumerate(data))
+working_list = list(indexed_data)
+n = len(data)
+for item in indexed_data:
+    val = item[1]
+    if val == 0:
+        continue
+    old_idx = working_list.index(item)
+    working_list.pop(old_idx)
+    new_idx = (old_idx + val) % (n - 1)
+    working_list.insert(new_idx, item)
+final_values = [x[1] for x in working_list]
+zero_idx = final_values.index(0)
+ans = sum(final_values[(zero_idx + i) % n] for i in [1000, 2000, 3000])
+print(ans)
